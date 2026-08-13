3a8c771d5866abca72756184378fdc725da88c35
RayhanHanun <rayhanhanun@students.amikom.ac.id>
2026-08-13 23:03:46 +0700

    Perf: Optimize package imports, add browserslist, and set font display swap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
   subsets: ["latin"],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
